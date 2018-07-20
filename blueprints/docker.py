4a20de3a0b6ca44d36c5753d0b7c731f3538694b
FurryousCoyote <mtancredi@gmail.com>
2018-07-20 19:57:37 -0300

    first commit

---
+++ blueprints/docker.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+docker = Blueprint('docker', __name__, url_prefix='/docker')
+
+@docker.route('')
+def home():
+    return render_template('docker.html')
